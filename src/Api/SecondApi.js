c588fc608e377fbcb3f8968565091e1142a1f07f
Asit-website <asitmandal492@gmail.com>
2022-02-10 16:34:09 +0530

    fileupload

---
+++ src/Api/SecondApi.js
@@ -0,0 +1,33 @@
+const SecondApi = [
+    {
+        id:1,
+        subject:"Maths"
+    }
+    ,
+    {
+        id:2,
+        subject:"Science"
+    }
+    ,
+    {
+        id:3,
+        subject:"English"
+    }
+    ,
+    {
+        id:4,
+        subject:"History"
+    }
+    ,
+    {
+        id:5,
+        subject:"Geography"
+    }
+    ,
+    {
+        id:6,
+        subject:"Computer Science"
+    }
+]
+
+export default SecondApi;
